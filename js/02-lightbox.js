798fe5586b83c81eba0ab197cbc1bc0df5180507
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2023-11-20 11:01:21 +0100

    change lightbox js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,24 +10,19 @@ const items = addGalleryItems(galleryItems);
 gallery.insertAdjacentHTML('beforeend', items);
 
 function addGalleryItems(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    /></a></div>`;
-    })
-    .join('');
-}
+  return items.map(({ preview, original, description }) => {
+        return `<li>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}"  alt="${description}"/>
+     </a></li>`
+     }).join('');
+        }
 
 
 
   
    const lightbox = new SimpleLightbox('.gallery a', {
-       captionsData: 'alt',
+       
        captionPosition: 'bottom',
        captionDelay: 250,
        
